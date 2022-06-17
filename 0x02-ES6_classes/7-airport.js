a868518ac5f256faace24b350574a7500ca2e8e8
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-17 03:22:31 +0300

    :memo: Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (!(typeof name === 'string')) {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (!(typeof code === 'string')) {
+      throw new Error('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
