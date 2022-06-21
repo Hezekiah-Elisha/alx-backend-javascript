29e9f900d17996b56e5e568c86805037f8e47eb9
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-22 00:30:49 +0300

    :memo: Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (students instanceof Array) {
+    const defaultGrade = { grade: 'N/A' };
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
+}
