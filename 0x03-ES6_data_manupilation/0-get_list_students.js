0122aff880a4db232a16680950ab8341d638d90e
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-21 20:55:12 +0300

    :memo: Basic List of Objects

--- 0x03-ES6_data_manupilation/0-get_list_students.js
+++ 0x03-ES6_data_manupilation/0-get_list_students.js
@@ -1,18 +1,7 @@
-export default function getListStudents(id, firstName, location)
-{
-    return new Promise((resolve, reject) => {
-        let url = "http://localhost:3000/students";
-        let params = {
-            id: id,
-            firstName: firstName,
-            location: location
-        };
-        axios.get(url, {params: params})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
