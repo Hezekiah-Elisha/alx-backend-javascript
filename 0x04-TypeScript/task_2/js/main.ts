9ab62fe697a7b9c3934e3347c77409c72dca0355
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-23 02:40:33 +0300

    :memo: String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -49,9 +49,20 @@ export function executeWork(employee:(Director | Teacher)){
     return (employee as Teacher).workTeacherTasks();
 }
 
+type Subjects = ('Math' | 'History');
+export function teachClass(todayClass:Subjects){
+    if (todayClass === 'Math'){
+        return 'Teaching Math';
+    }
+    return 'Teaching History';
+}
+
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
