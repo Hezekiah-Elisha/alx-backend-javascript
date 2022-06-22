25a993b48dfe3337ea91d530bbfc7d49dbed04c1
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-23 01:37:30 +0300

    :memo: Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,6 +14,35 @@ export function printTeacher(firstName: string, lastName:string):string {
   return `${firstName.slice(0, 1)}. ${lastName}`;
 }
 
+
+export interface IStudentClassConstructor {
+  firstName: string
+  lastName: string
+}
+
+export interface IStudentClass extends IStudentClassConstructor {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass{
+  firstName!: string;
+  lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Curently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+} 
+
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -33,3 +62,6 @@ const director1: Directors = {
 console.log(director1);
 
 console.log(printTeacher("John", "Doe"));
+
+const Info = new StudentClass(director1.firstName, director1.lastName);
+console.log(Info.workOnHomework());
